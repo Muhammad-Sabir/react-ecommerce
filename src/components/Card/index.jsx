c8299ee492274dfffcb716160db900fb9aca692d
Muhammad Sabir <sabir.tabi1122@gmail.com>
2023-08-10 11:55:39 +0500

    Add Card component code

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const Card = () => {
-	return <></>;
+import classes from './styles.module.css';
+
+const Card = (props) => {
+	// props - className
+
+	return (
+		<div className={`${classes['card']} ${props.className}`}>
+			{props.children}
+		</div>
+	);
 };
 
 export default Card;

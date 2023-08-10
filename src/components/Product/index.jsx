e45f9f1daf919c6dbf503a3a792b2fa7597953d0
Muhammad Sabir <sabir.tabi1122@gmail.com>
2023-08-10 19:48:19 +0500

    Update Product component to make it dynamic using props

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -6,18 +6,15 @@ import classes from './styles.module.css';
 const Product = ({ title, price, category, imageLink }) => {
 	return (
 		<Card className={classes['product']}>
-			<img
-				src="https://assets.website-files.com/649409997a1a98426b1aa230/6494209ae4949b66bd9c69f1_Earbud%20Y168A-p-500.jpg"
-				alt="something"
-			/>
+			<img src={imageLink} alt={title} />
 
 			<div className={classes['product__details']}>
-				<p className={classes['product__details__category']}>Earbuds</p>
-				<h2 className={classes['product__details__title']}>
-					Earbud 168A
-				</h2>
+				<p className={classes['product__details__category']}>
+					{category}
+				</p>
+				<h2 className={classes['product__details__title']}>{title}</h2>
 				<p className={classes['product__details__price']}>
-					$ 87.24 USD
+					$ {price} USD
 				</p>
 			</div>
 		</Card>

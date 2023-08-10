2a8b5d3b46c65a694243edce50ba8527924190f7
Muhammad Sabir <sabir.tabi1122@gmail.com>
2023-08-10 11:29:01 +0500

    Add code for Button component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const Button = () => {
-	return <></>;
+import classes from './Button.module.css';
+
+const Button = (props) => {
+	// props - classname(optional), type(optional), onClick()
+
+	return (
+		<button
+			className={`${classes['button']} ${props.className}`}
+			type={props.type || 'button'}
+			onClick={props.onClick}
+		>
+			{props.children}
+		</button>
+	);
 };
 
 export default Button;

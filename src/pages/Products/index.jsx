54d118351b9c36a3ea2a1ba3a7d1cf6a65b1bb7b
Muhammad Sabir <sabir.tabi1122@gmail.com>
2023-08-13 15:18:17 +0500

    Stucture Product page and style input and search

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -33,30 +33,42 @@ const Products = () => {
 	return (
 		<div className={classes['products-page']}>
 			<div className={classes['products-page__search-filter']}>
+				<select
+					name="sort"
+					defaultValue=""
+					className={classes['select']}
+				>
+					<option value="" disabled>
+						Sort Products
+					</option>
+					<option value="price-ascending">Price Ascending</option>
+					<option value="price-descending">Price Descending</option>
+					<option value="alphabets-ascending">
+						Alphabets Ascending
+					</option>
+					<option value="alphabets-descending">
+						Alphabets Descending
+					</option>
+				</select>
+
 				<input
-					class="input"
+					className={classes['input']}
 					name="text"
 					placeholder="Search..."
 					type="search"
 				/>
-
-				<select name="sort" id="sort">
-					<option value="" disabled>Sort Products</option>
-					<option value="price-ascending">Price Ascending</option>
-					<option value="price-descending">Price Descending</option>
-					<option value="alphabets-ascending">Alphabets Ascending</option>
-					<option value="alphabets-descending">Alphabets Descending</option>
-				</select>
 			</div>
 
-			<CategorySidebar
-				products={products}
-				className={classes['products-page__sidebar']}
-			></CategorySidebar>
-			<ProductGrid
-				products={products}
-				className={classes['products-page__products-grid']}
-			></ProductGrid>
+			<div className={classes['products-page__category-products']}>
+				<CategorySidebar
+					products={products}
+					className={classes['products-page__sidebar']}
+				></CategorySidebar>
+				<ProductGrid
+					products={products}
+					className={classes['products-page__products-grid']}
+				></ProductGrid>
+			</div>
 		</div>
 	);
 };

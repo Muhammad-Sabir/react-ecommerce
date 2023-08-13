c891990b3d1a57645351293a45417238715e883c
Muhammad Sabir <sabir.tabi1122@gmail.com>
2023-08-13 14:16:49 +0500

    Add Navbar component code

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,7 +3,22 @@ import React from 'react';
 import classes from './styles.module.css';
 
 const Navbar = () => {
-	return <></>;
+	return (
+		<nav>
+			<div className="logo">E-COMMERCE</div>
+			<ul className="nav-links">
+				<li>
+					<a href="#">Home</a>
+				</li>
+				<li>
+					<a href="#">Products</a>
+				</li>
+				<li>
+					<a href="#">About</a>
+				</li>
+			</ul>
+		</nav>
+	);
 };
 
 export default Navbar;

a0808f2c3997e14dee291233b6fc8d4d1402da22
Muhammad Sabir <sabir.tabi1122@gmail.com>
2023-08-13 13:07:59 +0500

    Move custom loader and products state from ProductGrid to Products page

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import ProductGrid from '../../components/ProductGrid';
 
 import classes from './styles.module.css';
 
 const Products = () => {
-	return <ProductGrid></ProductGrid>;
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get('https://fakestoreapi.com/products')
+			.then((response) => {
+				setProducts(response.data);
+			})
+			.catch((error) => {
+				console.error('Error fetching products:', error);
+			});
+	}, []);
+
+	console.log(products);
+
+	if (!products.length) {
+		return (
+			<div className={classes['custom-loader-container']}>
+				<div className={classes['custom-loader']}></div>
+			</div>
+		);
+	}
+
+	return <ProductGrid products={products}></ProductGrid>;
 };
 
 export default Products;

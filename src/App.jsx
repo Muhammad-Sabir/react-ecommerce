6543159f2432d7ac4a29d7f15392290adfb2d97d
Muhammad Sabir <sabir.tabi1122@gmail.com>
2023-08-16 21:49:52 +0500

    Add outlet in navbar and adjust the routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Products from './pages/Products';
-import Categories from './pages/Categories';
 import Error from './pages/Error';
 
 import Login from './pages/Login';
@@ -14,18 +13,17 @@ import './App.css';
 function App() {
 	return (
 		<>
-			{/* <Navbar />
-
 			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="products" element={<Products />}>
-					<Route path=":categoryParam" element={<Products />} />
+				<Route path="/" element={<Navbar />}>
+					<Route index element={<Home />} />
+					<Route path="products" element={<Products />}>
+						<Route path=":categoryParam" element={<Products />} />
+					</Route>
 				</Route>
-				<Route path="categories" element={<Categories />} />
-				<Route path="*" element={<Error />} />
-			</Routes> */}
 
-			<Login></Login>
+				<Route path="login" element={<Login />} />
+				<Route path="*" element={<Error />} />
+			</Routes>
 		</>
 	);
 }

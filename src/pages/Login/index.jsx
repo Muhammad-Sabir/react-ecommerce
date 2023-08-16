91b666ff20348bdd1e30ce07124e4b380e9f5a14
Muhammad Sabir <sabir.tabi1122@gmail.com>
2023-08-16 18:58:33 +0500

    Add yup validation on the formik form

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -5,18 +5,55 @@ import * as yup from 'yup';
 import classes from './styles.module.css';
 
 const Login = () => {
+	const defaultValues = {
+		email: '',
+		password: '',
+	};
+
+	const validationSchema = yup.object().shape({
+		email: yup.string().required('Required').email('Invalid email'),
+		password: yup.string().required('Required'),
+	});
+
+	const handleSubmit = (values) => {
+		console.log(values);
+	};
+
 	return (
-		<Formik>
+		<Formik
+			initialValues={defaultValues}
+			validationSchema={validationSchema}
+			onSubmit={handleSubmit}
+		>
 			<Form className={classes['form']}>
 				<p className={classes['form-title']}>Sign in to your account</p>
-				
+
 				<div className={classes['input-container']}>
-					<Field type="email" placeholder="Enter email" />
-					<span></span>
+					<Field
+						type="email"
+						name="email"
+						placeholder="Enter email"
+					/>
+					<ErrorMessage
+						name="email"
+						component="div"
+						className={classes['error-message']}
+					/>
 				</div>
+
 				<div className={classes['input-container']}>
-					<Field type="password" placeholder="Enter password" />
+					<Field
+						type="password"
+						name="password"
+						placeholder="Enter password"
+					/>
+					<ErrorMessage
+						name="password"
+						component="div"
+						className={classes['error-message']}
+					/>
 				</div>
+
 				<button type="submit" className={classes['submit']}>
 					Sign in
 				</button>

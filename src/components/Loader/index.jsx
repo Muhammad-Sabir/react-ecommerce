63e57d66242e346fb8f12185026543658aa8694d
Muhammad Sabir <sabir.tabi1122@gmail.com>
2023-08-18 09:53:32 +0500

    Add jsx code for loader

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Loader = () => {
-	return <div>Loader</div>;
+	return (
+		<div className={classes['custom-loader-container']}>
+			<div className={classes['custom-loader']}></div>
+		</div>
+	);
 };
 
 export default Loader;

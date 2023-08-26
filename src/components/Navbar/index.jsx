0989cb1b632516d3711beb615d8092b4ee2f43ab
Muhammad Sabir <sabir.tabi1122@gmail.com>
2023-08-26 21:35:13 +0500

    Add cart count right next to cart button

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
@@ -11,6 +12,19 @@ import classes from './styles.module.css';
 const Navbar = () => {
 	const navigate = useNavigate();
 	const authContext = useContext(AuthContext);
+
+	const getProductsCount = () => {
+		const products = useSelector((state) => state.cart.products);
+
+		const count = products.reduce(
+			(total, product) => total + product.quantity,
+			0
+		);
+
+		return count;
+	};
+
+	const productsOnCartCount = getProductsCount();
 
 	const cartHandler = () => {
 		navigate('/cart');
@@ -40,6 +54,9 @@ const Navbar = () => {
 							className={classes['cart-btn']}
 						>
 							<FontAwesomeIcon icon={faShoppingCart} />
+							<span className={classes['cart-btn__prods-count']}>
+								{productsOnCartCount}
+							</span>
 						</div>
 
 						<div className={classes['authentication-btn']}>
